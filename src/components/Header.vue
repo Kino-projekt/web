546e2e95ba46d1b84479d0dc12bd4efaabd1e2b3
Przemysław Jaśkiewicz <przemo95732@gmail.com>
2020-05-15 02:32:35 +0200

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header> 
+<h1> {{title}} </h1>
+ </header>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            title: 'Scruter'
+        }
+    }
+ 
+}
+</script>
+
+<style scoped>
+header{
+    background: #A9A9A9 ;
+    padding: 10px;
+}
+h1{
+    color: #6495ED;
+    text-align: center;
+}
+</style>
