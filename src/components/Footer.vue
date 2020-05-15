400e9d87c163d9f979c6e04c00b38cc7ae97455f
Przemysław Jaśkiewicz <przemo95732@gmail.com>
2020-05-15 02:38:20 +0200

    footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer> 
+<p> {{copyright}} </p>
+ </footer>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            copyright: 'Copyright 2020 Scruter'
+        }
+    }
+ 
+}
+</script>
+
+<style scoped>
+footer{
+    background: #A9A9A9 ;
+    padding: 6px;
+}
+p{
+    color: #6495ED;
+    text-align: center;
+}
+</style>
